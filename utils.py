fe9eab2520886b40b8779fefdc3570797ce90eee
Hiro <goferHiroHiro@gmail.com>
2023-04-17 07:39:18 +0530

    :fire: submodules
    Signed-off-by: Hiro <goferHiroHiro@gmail.com>

--- utils.py
+++ utils.py
@@ -1,113 +1,107 @@
-
 """
 Additional Class for Kafka
 """
 
-from dataclasses import dataclass
-from typing import List,Callable,Optional
+import time
+from datetime import timedelta
+from typing import List, Optional
 
 from confluent_kafka import KafkaException
-import logging as log
 
 from .enums import *
-
 from .thread import keepAlive
 
-from datetime import timedelta
 
-import time
-
-
-def delivery_report(err:str,msg:object)->None:
+def delivery_report(err: str, msg: object) -> None:
     if err is not None:
         err_msg = f'Message delivery failed: {err}'
-        with open("delivery.log","a") as f1:
-          f1.write(err_msg)
+        with open("delivery.log", "a") as f1:
+            f1.write(err_msg)
     else:
-        with open("delivery.log","a") as f1:
-          f1.write(f'message delivered to {msg.topic()} [{ msg.partition()}]')
-  
+        with open("delivery.log", "a") as f1:
+            f1.write(f'message delivered to {msg.topic()} [{msg.partition()}]')
 
-#To be inherits to kafka
+
+# To be inherits to kafka
 class kafkaUtils():
+    context: bool = True
 
-  context:bool = True
-  
-  def create_topics(self,topics:List[str]): #todo add more sophistications later
-    from confluent_kafka.admin import NewTopic
+    def create_topics(self, topics: List[str]):  # todo add more sophistications later
+        from confluent_kafka.admin import NewTopic
 
-    admin_client = self.admin_client or self.admin().admin_client
-    num_partition = 6
-    replication_factor = 3 #minimum
-    
-    new_topics = [NewTopic(topic, num_partitions=num_partition, replication_factor=replication_factor) for topic in topics]
-    futures = admin_client.create_topics(new_topics)
-    for topic, future in futures.items():
-        try:
-            future.result()
-            log.info(f"Topic {topic} created")
-        except KafkaException as e:
-            log.error(f"Failed to create topic {topic}: {e}")
+        admin_client = self.admin_client or self.admin().admin_client
+        num_partition = 6
+        replication_factor = 3  # minimum
 
-  @staticmethod
-  def Delivery_report(err:str, msg:object)->None:
-     return delivery_report(err,msg)
-  
-  def publish(self,message:KafkaMessage,callback:Callable[[str,object],None] = delivery_report): 
-    producer = self.publisher or self.producer().publisher
+        new_topics = [NewTopic(topic, num_partitions=num_partition, replication_factor=replication_factor) for topic in
+                      topics]
+        futures = admin_client.create_topics(new_topics)
+        for topic, future in futures.items():
+            try:
+                future.result()
+                log.info(f"Topic {topic} created")
+            except KafkaException as e:
+                log.error(f"Failed to create topic {topic}: {e}")
 
-    topic = message.topic
-    key = message.key
-    value = message.val
-    producer.produce(topic, key=key, value=value,callback=callback)
-    producer.flush()
-    log.info(f"produced {message}")
+    @staticmethod
+    def Delivery_report(err: str, msg: object) -> None:
+        return delivery_report(err, msg)
 
-  @keepAlive
-  def stop_consumer(self,after:timedelta=timedelta(hours=4))->None:
-    log.info(f"killing consumer after {after}")
-    time.sleep(after.total_seconds())
-    self.context = False
-    log.info(f"killed consumer after {after}")
+    def publish(self, message: KafkaMessage, callback: Callable[[str, object], None] = delivery_report):
+        producer = self.publisher or self.producer().publisher
 
-  def consume(self,consumer_ppt:Optional[ConsumerProperties]=None)->None:
-    consumer_ppt = consumer_ppt or self.consumer_ppt 
+        topic = message.topic
+        key = message.key
+        value = message.val
+        producer.produce(topic, key=key, value=value, callback=callback)
+        producer.flush()
+        log.info(f"produced {message}")
 
-    self.context = True
+    @keepAlive
+    def stop_consumer(self, after: timedelta = timedelta(hours=4)) -> None:
+        log.info(f"killing consumer after {after}")
+        time.sleep(after.total_seconds())
+        self.context = False
+        log.info(f"killed consumer after {after}")
 
-    if consumer_ppt==None:
-      raise ValueError("ConsumerProperties not found")
+    def consume(self, consumer_ppt: Optional[ConsumerProperties] = None) -> None:
+        consumer_ppt = consumer_ppt or self.consumer_ppt
 
-    consumer = self.subscriber  or self.consumer(consumer_ppt).subscriber
-    timeout = consumer_ppt.poll_timeout
+        self.context = True
 
-    while self.context:
-        try:
-            msg = consumer.poll(timeout)
+        if consumer_ppt == None:
+            raise ValueError("ConsumerProperties not found")
 
-            if msg is None:
-                log.debug("Invalid Kafka message")
-                continue
+        consumer = self.subscriber or self.consumer(consumer_ppt).subscriber
+        timeout = consumer_ppt.poll_timeout
 
-            if msg.error():
-                log.error(f"Kafka failed to deliver message: {msg.error()}")
-                time.sleep(timeout * 20)
-                continue
+        while self.context:
+            try:
+                msg = consumer.poll(timeout)
 
-            key = msg.key().decode("utf-8")
-            val = msg.value().decode("utf-8")
-            consumer_ppt.callback(key, val)
+                if msg is None:
+                    log.debug("Invalid Kafka message")
+                    continue
 
-        except KafkaException as e:
-            log.error(f"Kafka failed to subscribe: {consumer_ppt} due to: {e}")
+                if msg.error():
+                    log.error(f"Kafka failed to deliver message: {msg.error()}")
+                    time.sleep(timeout * 20)
+                    continue
 
-        except KeyboardInterrupt:
-            log.debug("Received KeyboardInterrupt")
-            break
+                key = msg.key().decode("utf-8")
+                val = msg.value().decode("utf-8")
+                consumer_ppt.callback(key, val)
 
-        except Exception as e:
-            log.error(f"An error occurred: {e}")
-            break
+            except KafkaException as e:
+                log.error(f"Kafka failed to subscribe: {consumer_ppt} due to: {e}")
 
-    consumer.close()
-    log.info("Consumer closed")
+            except KeyboardInterrupt:
+                log.debug("Received KeyboardInterrupt")
+                break
+
+            except Exception as e:
+                log.error(f"An error occurred: {e}")
+                break
+
+        consumer.close()
+        log.info("Consumer closed")
